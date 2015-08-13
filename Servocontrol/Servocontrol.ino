6aa8b7d1fe1488124b098d94950376ee2a41bd45
noBlubb <raffael.stahl@gmail.com>
2015-08-13 20:09:21 +0200

    weiterarbeit

--- Servocontrol/Servocontrol.ino
+++ Servocontrol/Servocontrol.ino
@@ -5,14 +5,14 @@
 
 Servo motor, servoR, servoL, servoH, servoT;     // create servo object (Motordrehzahl, Taumelscheibe Rechts, Taumelscheibe Links, Taumelscheibe Hinten, Heckrotor)
 
-int nick = 90; //Neigung nach vorne
+int nick = 90;  //Neigung nach vorne
 int roll = 90;  //Neigung nach rechts
 int coll = 90;  //Kollektive Blattverstellung
 int yaw = 90;   //Drehung um Rotorachse, in Servo-Form
 
-int servoRval = 0;
-int servoLval = 0;
-int servoHval = 0;
+int servoRval = 90;
+int servoLval = 90;
+int servoHval = 90;
 
 void setup()
 {
@@ -29,16 +29,16 @@ void setup()
   servoT.write(90);
   delay(5000);
   
-  servoR.write(140);
-  servoL.write(40);
-  servoH.write(40);
-  servoT.write(40);
+  servoR.write(130);
+  servoL.write(50);
+  servoH.write(50);
+  servoT.write(50);
   delay(2000);
   
-  servoR.write(40);
-  servoL.write(140);
-  servoH.write(140);
-  servoT.write(140);
+  servoR.write(50);
+  servoL.write(130);
+  servoH.write(130);
+  servoT.write(130);
   delay(2000);
   
 }
@@ -48,42 +48,57 @@ void loop()
   //ToDo(da): wenn kein Input, dann aktive Stabilisierung -> Algorithmus mit Output nick, roll, coll, yaw
   //ToDo(da): Input bekommen -> Interrupt? kphochdrölf
   //Output in nick, roll, coll, yaw
-    
-  for(int x=0; x<10; x++){
-    nick = 10 * x + 40;
-       
-    for(int y=0; y<10; y++){
-      roll = 10 * y + 40;
-      
-      for(int z=0; z<10; z++){
-        coll = 10 * z + 40;
-        execute();
-        delay(100);
-      }
-    } 
-    
-  }
   
+  nick=90;  
+  roll=90;
+  coll=90;
+  execute();
+  delay(5000);
+  
+  nick=130;  
+  roll=90;
+  coll=90;
+  execute();
+  delay(5000);
+  
+  nick=90;  
+  roll=130;
+  coll=90;
+  execute();
+  delay(5000);
+  
+  nick=90;  
+  roll=90;
+  coll=130;
+  execute();
+  delay(5000);
+  
+  nick=110;  
+  roll=110;
+  coll=110;
+  execute();
+  delay(5000);
+    
 } 
 
 void execute(){
   
-  servoRval = (nick-90) / (-2) + (-1) * (roll-90) + (coll-90) + 90;
+  servoRval = (nick-90) / (-2) + (roll-90) + (-1) * (coll-90) + 90;
   servoLval = (nick-90) / (-2) + (roll-90) + (coll-90) + 90;
   servoHval = (nick-90) + (coll-90) + 90;
 
-  if(servoRval < 70)  { servoRval = 70;  }
-  if(servoRval > 110) { servoRval = 110; }
-  if(servoLval < 70)  { servoLval = 70;  }
-  if(servoLval > 110) { servoLval = 110; }
-  if(servoHval < 70)  { servoHval = 70;  }
-  if(servoHval > 110) { servoHval = 110; }
-  if(yaw < 70)  { yaw = 70;  }
-  if(yaw > 110) { yaw = 110; }
+  if(servoRval < 80)  { servoRval = 80;  }
+  if(servoRval > 120) { servoRval = 120; }
+  if(servoLval < 80)  { servoLval = 80;  }
+  if(servoLval > 120) { servoLval = 120; }
+  if(servoHval < 80)  { servoHval = 80;  }
+  if(servoHval > 120) { servoHval = 120; }
+  if(yaw < 60)  { yaw = 60;  }
+  if(yaw > 120) { yaw = 120; }
 
-  servoR.write(servoRval);
-  servoL.write(servoLval);
-  servoH.write(servoHval);
+  servoR.write(180-servoRval);
+  servoL.write(180-servoLval);
+  servoH.write(180-servoHval);
 
   servoT.write(yaw);
 }
